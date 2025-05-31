9f26550c45d825363710a4de1b4f4ef0b8509036
Raihan Nismara <raihannismara@gmail.com>
2025-05-31 19:00:29 +0700

    chore: add ut, synx, fix structure github actions

---
+++ app.test.js
@@ -0,0 +1,13 @@
+describe('testing artimatika', () => {
+  it('should return 4 when adding 2 and 2', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it('should return 5 when adding 2 and 3', () => {
+    expect(2 + 3).toBe(5);
+  });
+
+  it('should return 6 when adding 3 and 3', () => {
+    expect(3 + 3).toBe(6);
+  });
+});
